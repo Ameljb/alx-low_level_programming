1f29fbc90f24a4567435e443b6c4d2e0e9ad8f50
Amel Jaballah <jaballahamel00@gmail.com>
2023-09-08 09:42:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+
 /**
- * The main function 
- * return 0 on successful execution.
- */
+ * main - this is the main function
+ * Return: always 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
