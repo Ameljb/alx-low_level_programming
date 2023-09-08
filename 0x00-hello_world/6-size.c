4ba7f261b6f67a116132577da9636a242ff7f1e9
Amel Jaballah <jaballahamel00@gmail.com>
2023-09-08 10:02:39 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,22 @@
-#include <stadio.h>
-/**
- * *main - A program that prints the size of varius computer types 
+#include <stdio.h>
 
- * RETURN 0 (success)
- */
+/**
+ *main - Entry point
+ *Description : using sizeof to print the size of various type .
+ * RETURN: always 0 (success)
+*/
+
 int main(void)
 {
 char a;
 int b;
 long int c;
-long long int d; 
+long long int d;
 float f;
-printf("size of char :%lu byte(s)\n", (unsigned sizeof(a));
-printf("size of int :%lu byte(s)\n", (unsigned sizeof(b));		
-printf("size of  long int :%lu byte(s)\n", (unsigned sizeof(c));
-
-printf("size of long  long int :%lu byte(s)\n", (unsigned sizeof(d));
-printf("size of float:%lu byte(s)\n", (unsigned sizeof(f));
+printf("size of char :%lu byte(s)\n", (sizeof(a));
+printf("size of int :%lu byte(s)\n", (sizeof(b);
+printf("size of  long int :%lu byte(s)\n", (sizeof(c));
+printf("size of long  long int :%lu byte(s)\n", (sizeof(d));
+printf("size of float:%lu byte(s)\n", (sizeof(f));
 return (0);
 }
